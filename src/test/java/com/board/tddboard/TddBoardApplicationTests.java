490471f746d9f46fff3967b0416c7625da0ef67d
Beoum <qoqjagml123@naver.com>
2021-06-27 19:20:20 +0900

    project init

---
+++ src/test/java/com/board/tddboard/TddBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.tddboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
